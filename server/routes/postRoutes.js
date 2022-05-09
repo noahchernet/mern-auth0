577ed9f72f63dcf445c63e75736b91ecd7c3b85d
Noah Chernet <noahchernet@yahoo.com>
2022-05-09 15:45:56 +0300

    Created all API endpoints to access the posts

---
+++ server/routes/postRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+import {
+  create,
+  likePost,
+  deleteAll,
+  deletePost,
+  update,
+  findOne,
+  findAll,
+} from "../controller/postController.js";
+
+router = express.Router();
+
+export default (app) => {
+  router.post("/", create);
+  router.get("/", findAll);
+  router.get("/:id", posts.findOne);
+  router.put("/:id", update);
+  router.delete("/:id", deletePost);
+  router.delete("/", deleteAll);
+  router.patch("/:id/likePost", likePost);
+  app.use("/api/posts", router);
+};
