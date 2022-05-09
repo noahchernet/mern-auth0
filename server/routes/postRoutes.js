cc8775610de87ea6b4131d381bcfa7be9b1ae69a
Noah Chernet <noahchernet@yahoo.com>
2022-05-09 20:23:35 +0300

    Updated routes to include JWT token validation middleware

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -8,16 +8,35 @@ import {
   findOne,
   findAll,
 } from "../controller/postController.js";
+import checkJWT from "../controller/checkJWT.js";
+import fileUpload from "../controller/fileUpload.js";
 
 router = express.Router();
 
 export default (app) => {
-  router.post("/", create);
+  // Create a new Post
+  router.post("/", [checkJWT, fileUpload.single("img")], create);
+
+  // Retrieve all Posts. No middleware needed since it's unnecessary when
+  // viewing posts
   router.get("/", findAll);
+
+  // Retrieves a single Post.
+  // No middleware needed since it's unnecessary when viewing posts
   router.get("/:id", findOne);
-  router.put("/:id", update);
-  router.delete("/:id", deletePost);
-  router.delete("/", deleteAll);
-  router.patch("/:id/likePost", likePost);
+
+  // Update a post with specified id
+  // fileUpload middleware needed since the post's image could get updated
+  router.put("/:id", [checkJWT, fileUpload.single("img")], update);
+
+  // Delete a Post with id
+  router.delete("/:id", checkJWT, deletePost);
+
+  // Delete all Posts
+  router.delete("/", checkJWT, deleteAll);
+
+  // Like a Post
+  router.patch("/:id/likePost", checkJWT, likePost);
+
   app.use("/api/posts", router);
 };
