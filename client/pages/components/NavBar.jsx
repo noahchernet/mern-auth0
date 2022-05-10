3f0de28fae2680ae3027ade9e0a4cd847ad57317
Noah Chernet <noahchernet@yahoo.com>
2022-05-10 10:22:18 +0300

    navbar completed and added SVGs for later use

--- client/pages/components/NavBar.jsx
+++ client/pages/components/NavBar.jsx
@@ -1,5 +1,9 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { NavLink } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import AuthenticationButton from "./AuthenticationButton.jsx";
+import AuthenticationButton from "./AuthenticationButton";
 
 const guestItem = [{ path: "/", title: "Home" }];
 
@@ -11,9 +15,33 @@ const authItem = [
 ];
 
 const NavBar = () => {
-  const {isAuthenticated} = useAuth0;
+  const { isAuthenticated } = useAuth0;
 
-  return ()
-}
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="ligth" variant="light">
+      <Container>
+        <Navbar.Brand href="#home" />
+        <Navbar.Toggle aira-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            {(isAuthenticated ? authItem : guestItem).map((item) => {
+              <NavLink
+                key={item.path}
+                to={item.path}
+                exact
+                className="nav-link"
+              >
+                {item.title}
+              </NavLink>;
+            })}
+          </Nav>
+          <Nav>
+            <AuthenticationButton />
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
 
-export default NavBar
+export default NavBar;
